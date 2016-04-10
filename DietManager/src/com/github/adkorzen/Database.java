6bed3e480c9a5bd400a3fb7f4ebbed27ce9e68f7
Adam Korzeniak <adkorzen@gmail.com>
2016-04-10 17:56:34 +0200

    added Listeners and some changes

---
+++ DietManager/src/com/github/adkorzen/Database.java
@@ -0,0 +1,19 @@
+package com.github.adkorzen;
+
+public class Database {
+	public void addToDatabase() {
+		System.out.println("added to database");
+	}
+
+	public void editDatabase() {
+		System.out.println("database edited");
+	}
+
+	public void checkDatabase() {
+		System.out.println("database checked");
+	}
+
+	public void proceed() {
+		System.out.println("Proceed to day menu");
+	}
+}
---
+++ DietManager/src/com/github/adkorzen/DateSetting.java
@@ -0,0 +1,16 @@
+package com.github.adkorzen;
+
+public class DateSetting {
+
+	public void openCalendar() {
+		System.out.println("Calendar opened");
+	}
+
+	public void setDateToday() {
+		System.out.println("date set to Today");
+	}
+
+	public void setDateYesterday() {
+		System.out.println("date set to Yesterday");
+	}
+}
