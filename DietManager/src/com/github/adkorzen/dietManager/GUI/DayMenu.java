e34aa0fa9da777794f453d200a574611a4eecba9
Adam Korzeniak <adkorzen@gmail.com>
2016-07-18 10:03:57 +0200

    Day Menu: Buttons added

--- DietManager/src/com/github/adkorzen/dietManager/GUI/DayMenu.java
+++ DietManager/src/com/github/adkorzen/dietManager/GUI/DayMenu.java
@@ -1,7 +1,12 @@
 package com.github.adkorzen.dietManager.GUI;
 
+import static com.github.adkorzen.dietManager.GUI.GUICoinstraints.setGUIConstraints;
+
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -23,9 +28,10 @@ import com.github.adkorzen.dietManager.GUI.CalendarView.Menu;
 
 public class DayMenu {
 	private static JFrame frame;
-	private JPanel northPanel;
+	private JPanel northPanel, southPanel;
 	private int monitorWidth, monitorHeight;
-	private static JButton dayBack, dayForward, datePicked, addMeal;
+	private GridBagConstraints c;
+	private static JButton dayBack, dayForward, datePicked, addMealButton, deleteButton, editButton;
 	private static DateFormat df;
 	private static JScrollPane scroll;
 	private static JTable table;
@@ -48,8 +54,24 @@ public class DayMenu {
 		northPanel.add(datePicked, BorderLayout.CENTER);
 		northPanel.add(dayForward, BorderLayout.LINE_END);
 		
-		addMeal = new JButton("Add Meal");
-		frame.getContentPane().add(addMeal, BorderLayout.PAGE_END);
+		southPanel = new JPanel();
+		southPanel.setLayout(new GridBagLayout());
+		
+		c = new GridBagConstraints();
+		
+		addMealButton = new JButton("Add Meal");
+		setGUIConstraints(c, 0, 0, 2, 1, GridBagConstraints.BOTH, new Insets(0, 10, 0, 10));
+		southPanel.add(addMealButton, c);
+		
+		deleteButton = new JButton("Delete");
+		setGUIConstraints(c, 0, 1, GridBagConstraints.BOTH, new Insets(10, 30, 10, 30));
+		southPanel.add(deleteButton, c);
+		
+		editButton = new JButton("Edit");
+		setGUIConstraints(c, 1, 1, GridBagConstraints.BOTH, new Insets(10, 30, 10, 30));
+		southPanel.add(editButton, c);
+		
+		frame.add(southPanel, BorderLayout.PAGE_END);
 		
 		table = new Table().getTable();
 		scroll = new JScrollPane(table);
